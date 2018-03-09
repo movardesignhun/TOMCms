ad5ef33d40321bb76a5583ba74b87fe6e50b9251
movardesignhun <info@movardesign.hu>
2018-03-09 22:57:28 +0100

    változtatások

--- public/js/app.js
+++ public/js/app.js
@@ -1053,6 +1053,10 @@ window.Vue = __webpack_require__(36);
 Vue.use(__WEBPACK_IMPORTED_MODULE_0_buefy___default.a);
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue'));
+var app = new Vue({
+   el: '#app',
+   data: {}
+});
 
 /***/ }),
 /* 12 */
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,3 +6,7 @@ import Buefy from 'buefy';
 Vue.use(Buefy);
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue'));
+var app = new Vue({
+   el: '#app',
+   data: {}
+})
